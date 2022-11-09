0f1feec475bc41096a578f3da575e6e108f4766d
mac.window.10 <mac.window.10@gmail.com>
2022-11-09 22:49:07 +0500

    more user registration fields, product images

--- database.py
+++ database.py
@@ -13,19 +13,19 @@ def create_db():
     con.commit()
 
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS product(id INTEGER PRIMARY KEY AUTOINCREMENT, category_id INTEGER, price REAL, loyalty_points INTEGER, name text)")
-    cur.execute("INSERT INTO product VALUES(1, 1, 20, 10, 'Panadol')")
-    cur.execute("INSERT INTO product VALUES(2, 1, 15, 5, 'Disprin')")
-    cur.execute("INSERT INTO product VALUES(3, 1, 40, 20, 'Ponston')")
-    cur.execute("INSERT INTO product VALUES(4, 2, 210, 30, 'Ponds Cream')")
-    cur.execute("INSERT INTO product VALUES(5, 2, 150, 35, 'Fair Menz Cream')")
+        "CREATE TABLE IF NOT EXISTS product(id INTEGER PRIMARY KEY AUTOINCREMENT, category_id INTEGER, price REAL, loyalty_points INTEGER, name text, image_path text)")
+    cur.execute("INSERT INTO product VALUES(1, 1, 20, 10, 'Panadol', 'panadol.jpg')")
+    cur.execute("INSERT INTO product VALUES(2, 1, 15, 5, 'Disprin', 'disprin.jpg')")
+    cur.execute("INSERT INTO product VALUES(3, 1, 40, 20, 'Ponston', 'ponston.jpg')")
+    cur.execute("INSERT INTO product VALUES(4, 2, 210, 30, 'Ponds Cream', 'ponds_cream.jpg')")
+    cur.execute("INSERT INTO product VALUES(5, 2, 150, 35, 'Fair Menz Cream', 'fair_menz_cream.jpg')")
     print('product')
     con.commit()
 
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTOINCREMENT, name text, password text, loyalty_points INTEGER)")
-    cur.execute("INSERT INTO user VALUES(1, 'john', '123456', 0)")
-    cur.execute("INSERT INTO user VALUES(2, 'david', '123456', 0)")
+        "CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTOINCREMENT, username text, name text, password text, dob text, gender text, address text, loyalty_points INTEGER)")
+    cur.execute("INSERT INTO user VALUES(1, 'kashif', 'Kashif', '123456', '01/07/1990', 'MALE', 'Lahore', 0)")
+    cur.execute("INSERT INTO user VALUES(2, 'zaheer', 'Zaheer', '123456', '02/09/1995', 'MALE', 'Lahore', 0)")
     print('user')
     con.commit()
 
